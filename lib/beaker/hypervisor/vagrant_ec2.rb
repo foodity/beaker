8aa9c6dab7bb6d752d91aee2adbe44a1e911b5ef
Claudio Benfatto <claudio.benfatto@foodity.com>
2015-04-14 09:13:36 +0100

    Changed options with host

--- lib/beaker/hypervisor/vagrant_ec2.rb
+++ lib/beaker/hypervisor/vagrant_ec2.rb
@@ -8,10 +8,10 @@ class Beaker::VagrantEc2 < Beaker::Vagrant
   def self.provider_vfile_section(host, options)
     provider_section  = ""
     provider_section << "    v.vm.provider :aws do |aws, override|\n"
-    provider_section << "      aws.access_key_id = #{options['aws_access_key_id']}"
-    provider_section << "      aws.secret_access_key = #{options['aws_secret_access_key']}"
-    provider_section << "      aws.keypair_name = \"#{options['aws_keypair_name']}\" "
-    provider_section << "      aws.ami = \"#{options['aws_ami']}\""
+    provider_section << "      aws.access_key_id = #{host['aws_access_key_id']}\n"
+    provider_section << "      aws.secret_access_key = #{host['aws_secret_access_key']}\n"
+    provider_section << "      aws.keypair_name = \"#{host['aws_keypair_name']}\"\n"
+    provider_section << "      aws.ami = \"#{host['aws_ami']}\"\n"
     provider_section << "    end\n"
 
     provider_section
