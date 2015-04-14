344d957584e3324f979e143edabcd86d35df84ab
Claudio Benfatto <claudio.benfatto@foodity.com>
2015-04-14 09:35:52 +0100

    Added parameters to vagrantec2

--- lib/beaker/hypervisor/vagrant_ec2.rb
+++ lib/beaker/hypervisor/vagrant_ec2.rb
@@ -8,10 +8,15 @@ class Beaker::VagrantEc2 < Beaker::Vagrant
   def self.provider_vfile_section(host, options)
     provider_section  = ""
     provider_section << "    v.vm.provider :aws do |aws, override|\n"
-    provider_section << "      aws.access_key_id = #{host['aws_access_key_id']}\n"
-    provider_section << "      aws.secret_access_key = #{host['aws_secret_access_key']}\n"
-    provider_section << "      aws.keypair_name = \"#{host['aws_keypair_name']}\"\n"
+    provider_section << "      aws.access_key_id = \"#{host['aws_access_key_id']}\"\n" unless host['aws_access_key_id'].nil?
+    provider_section << "      aws.secret_access_key = #{host['aws_secret_access_key']}\n" unless host['aws_secret_access_key'].nil?
+    provider_section << "      aws.keypair_name = \"#{host['aws_keypair_name']}\"\n" unless host['aws_keypair_name'].nil?
+    provider_section << "      instance_type = \"#{host['aws_instance_type'] ||= 't2.micro'}\"\n"
     provider_section << "      aws.ami = \"#{host['aws_ami']}\"\n"
+    provider_section << "      aws.region = \"#{host['aws_region'] ||= 'eu_west_1'}\"\n"
+    provider_section << "      aws.subnet_id = \"#{host['aws_subnet_id']}\"\n"
+    provider_section << "      aws.tags = \"#{host['aws_tags'] ||= {'Name' => 'Temporary'}}\"\n"
+
     provider_section << "    end\n"
 
     provider_section
